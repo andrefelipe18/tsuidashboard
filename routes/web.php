521d827731a8e4dcfa26cee474199260c7a96140
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-22 17:27:04 -0300

    Add FormLayout page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Volt::route('/', 'pages.dashboard')
     ->name('dashboard');
+
+
+Route::prefix('uikit')->group(function () {
+    Volt::route('/formlayout', 'pages.formlayout')
+        ->name('formlayout');
+});
